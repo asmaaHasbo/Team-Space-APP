32a992b35f6921cdb6be33467814b262e6285bd4
asmaa <elsaeeedasmaa@gmail.com>
2026-09-01 23:53:31 +0300

    feat: integrate Firebase core and messaging, update .gitignore and Gradle files

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
@@ -8,7 +9,7 @@ import 'package:team_space/team_space_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp();
   await EasyLocalization.ensureInitialized();
   await dotenv.load(fileName: '.env');
   await Supabase.initialize(
